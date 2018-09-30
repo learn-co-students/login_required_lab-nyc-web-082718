e68e98f1c08cb2349d0f59c3da1a92e1a99c7440
yapjinai <jinai.jyap@gmail.com>
2018-09-30 12:42:38 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def home
+  end
+
+  def secret
+  end
+
+  private
+
+  def require_login
+    redirect_to '/login' unless session[:name]
+  end
+
+end
